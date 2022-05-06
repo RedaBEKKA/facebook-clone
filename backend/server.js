59a87549f6bc056600c01a44b675af758cbb0ca3
RedaBEKKA <reda.bekka@gmail.com>
2022-05-07 00:52:00 +0200

    feat(backend):config backend

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
-
-// const options = {
-//   origin: "http://localhost:3000",
-//   useSuccessStatus: 200,
-// };
-// let allowed = ["http://localhost:3000", "http://localhost:3001"];
-// function options(req, res) {
-//   let tmp;
-//   let origin = req.header("Origin");
-//   if (allowed.indexOf(origin) > -1) {
-//     tmp = {
-//       origin: true,
-//       optionSuccessStatus: 200,
-//     };
-//   } else {
-//     tmp = {
-//       origin: false,
-//     };
-//   }
-//   res(null, tmp);
-// }
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(cors());
+// 1er méthode :
+// const useRoutes = require("./routes/user");
+// app.use("/", useRoutes);
+//2 eme methode :
+const { readdirSync, copyFileSync } = require("fs"); //Pour lire tous les fichiers d'un folder
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.get("/", (req, res) => {
-  res.send("Azul from Backend");
-});
-app.listen(8000, () => {
-  console.log("Server is listening..");
+//Database :
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database connected succesfully");
+  })
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
